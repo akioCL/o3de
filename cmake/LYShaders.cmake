a91da56a0c072cdd5dfc3b85c9f2839bc01300b1
Judy Ng <njud@amazon.com>
2021-06-24 22:42:31 -0700

    azsl file goes through MCPP and outputs the flat azsl file

--- cmake/LYShaders.cmake
+++ cmake/LYShaders.cmake
@@ -11,14 +11,15 @@
 
 #! ly_add_shader: adds a shader to be compiled
 function(ly_add_shader)
-
+    
+    # Parse the arguments for the function calls ------------------------------------------------------
     set(options)
     set(oneValueArgs NAME AZSL ENTRY_VS ENTRY_PS)
     set(multiValueArgs)
 
     cmake_parse_arguments(ly_add_shader "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Validate input arguments
+    # Validate input arguments ------------------------------------------------------------------------
     if(NOT ly_add_shader_NAME)
         message(FATAL_ERROR "You must provide a name for the target")
     endif()
@@ -28,11 +29,50 @@ function(ly_add_shader)
 
     set(AZSL_path ${CMAKE_CURRENT_LIST_DIR}/${ly_add_shader_AZSL})
 
+    # Send a message to ensure that we are compiling this shader
     if(EXISTS ${AZSL_path})
         message("Fake compiling shader ${ly_add_shader_NAME}")
     else()
         message(FATAL_ERROR "Shader source file ${ly_add_shader_AZSL} doesn't exist")
     endif() 
 
+    # Run the AZSL file through MCPP -----------------------------------------------------------------
+    set(MCPP "C:/3rdParty/packages/mcpp-2.7.2_az.1-rev1-windows/mcpp/lib/mcpp.exe")
+
+    set(inc_dirs 
+        ${LY_ROOT_FOLDER}/Gems/Atom/RPI/Assets/ShaderLib 
+        ${LY_ROOT_FOLDER}/Gems/Atom/Feature/Common/Assets/ShaderLib
+        ${LY_ROOT_FOLDER}/Gems/Atom/Feature/Common/Assets/ShaderResourceGroups
+    )
+    list(TRANSFORM inc_dirs PREPEND "-I")
+
+    # The file to be outputted by MCPP (the flat azsl file)
+    set(preprocessed_shader_path ${CMAKE_CURRENT_BINARY_DIR}/Shaders/${ly_add_shader_NAME}.azsl.in)
+
+    # Add the target for this shader
+    add_custom_target(${ly_add_shader_NAME} 
+        DEPENDS
+            ${preprocessed_shader_path}
+        SOURCES
+            ${AZSL_path}
+    )
+
+    # Add the command for this shader
+    add_custom_command(
+        OUTPUT 
+            ${preprocessed_shader_path}
+        COMMAND 
+            ${MCPP} ${inc_dirs} -C -w -+ ${AZSL_path} -o ${preprocessed_shader_path}
+        DEPENDS
+            ${AZSL_path}
+        COMMAND_EXPAND_LISTS
+    )
+    
+    # Move the target location to inside the designated folder in IDE
+    file(RELATIVE_PATH project_path ${LY_ROOT_FOLDER} ${CMAKE_CURRENT_LIST_DIR})
+    set(ide_path ${project_path})
+    set_target_properties(${ly_add_shader_NAME} PROPERTIES 
+        FOLDER "${ide_path}"
+    )
 
 endfunction()
